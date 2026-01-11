1e21e8bdc6d330ec6c80d46fdcd6f2746a3c4150
Malse <eslamwtf@gmail.com>
2026-01-11 23:27:56 +0200

    fixed the double slash

--- client/lib/api/client.ts
+++ client/lib/api/client.ts
@@ -3,7 +3,7 @@
 // lib/api/client.ts
 import axios from 'axios';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_BASE_URL = (process.env.NEXT_PUBLIC_API_URL || '').replace(/\/+$/, '');
 
 
 export const api = axios.create({
